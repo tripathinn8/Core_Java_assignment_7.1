d83b7b2b8b77cc0f271b0fbf0dc2aeb352f38e45
tripathinn8 <30956659+tripathinn8@users.noreply.github.com>
2017-08-25 23:19:06 +0530

    Add files via upload

---
+++ MyString.java
@@ -0,0 +1,11 @@
+
+public class MyString {
+
+static	int name=10;
+	public static void main(String[] args) {
+		String NewName=new String();
+		String a=NewName.valueOf(name);
+System.out.println(a);
+	}
+
+}
